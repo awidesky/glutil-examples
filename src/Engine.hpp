4b484bebcefcb337f171c4be375c7d020ec5d774
SSW20 <anfdmlrltk11@naver.com>
2026-05-27 17:44:58 +0900

    Make InputManager

--- src/Engine.hpp
+++ src/Engine.hpp
@@ -81,3 +81,48 @@ private:
 	float m_width = 0, m_height = 0;
 	std::string m_title;
 };
+
+class InputManager
+{
+public:
+	static InputManager& Get()
+	{
+		static InputManager input;
+		return input;
+	}
+
+	InputManager(const InputManager&) = delete;
+	InputManager& operator=(const InputManager&) = delete;
+
+	void Init(GLFWwindow* window)
+	{
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		glfwSetCursorPosCallback(window, MouseCallback);
+	}
+
+	static void MouseCallback(GLFWwindow* window, double x, double y)
+	{
+		InputManager::Get().OnMouseMove(x, y);
+	}
+	void OnMouseMove(double x, double y) {
+		if (m_firstMouse) {
+			m_lastX = x;
+			m_lastY = y;
+			m_firstMouse = false;
+		}
+		m_dX = x - m_lastX;
+		m_dY = m_lastY - y;  
+		m_lastX = x;
+		m_lastY = y;
+	}
+
+	std::pair<float, float> GetMouseDelta() const {
+		return { m_dX, m_dY };
+	}
+
+private:
+	InputManager() = default;
+	double m_lastX = 0, m_lastY = 0;
+	double m_dX = 0, m_dY = 0;
+	bool m_firstMouse = true;
+};
