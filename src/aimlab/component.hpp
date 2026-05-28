4338f19370c610d165d8db3f025e88a92ac91630
SSW20 <anfdmlrltk11@naver.com>
2026-05-28 22:37:32 +0900

    Add Transform, GameObject, Component Class

---
+++ src/aimlab/component.hpp
@@ -0,0 +1,81 @@
+﻿#ifndef COMPONENT_HPP
+#define COMPONENT_HPP
+
+#pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+struct Transform {
+    glm::vec3 position{0, 0, 0};
+    glm::vec3 rotation{0, 0, 0};
+    glm::vec3 scale{1, 1, 1};
+
+    glm::mat4 GetWorldMatrix() const {
+        glm::mat4 m(1.f);
+        m = glm::translate(m, position);
+        m = glm::rotate(m, glm::radians(rotation.x), {1, 0, 0});
+        m = glm::rotate(m, glm::radians(rotation.y), {0, 1, 0});
+        m = glm::rotate(m, glm::radians(rotation.z), {0, 0, 1});
+        return glm::scale(m, scale);
+    }
+};
+
+class GameObject;
+class Component {
+public:
+    GameObject* pOwner = nullptr;
+    bool isStarted = false;
+    virtual void Start() {}
+    virtual void Update(float dt) {}
+    virtual void Render() {}
+    virtual ~Component() {}
+};
+
+
+class GameObject {
+public:
+    Transform transform;
+    bool active = true;
+
+    ~GameObject() {
+        for (auto c : m_components)
+            delete c;
+    }
+
+    void AddComponent(Component* c) {
+        c->pOwner = this;
+        m_components.push_back(c);
+    }
+
+    template <typename T> T* GetComponent() {
+        for (auto c : m_components)
+            if (auto t = dynamic_cast<T*>(c))
+                return t;
+        return nullptr;
+    }
+
+    void Update(float dt) {
+        if (!active)
+            return;
+        for (auto c : m_components) {
+            if (!c->isStarted) {
+                c->Start();
+                c->isStarted = true;
+            }
+            c->Update(dt);
+        }
+    }
+
+    void Render() {
+        if (!active)
+            return;
+        for (auto c : m_components)
+            c->Render();
+    }
+
+private:
+    std::vector<Component*> m_components;
+};
+
+
+#endif // COMPONENT_HPP
